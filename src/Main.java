950263fa9ad3f7983b8e180e30456550c4642722
Roki Wijaya <rokiwijaya09@gmail.com>
2026-06-20 17:05:49 +0700

    Added turn based battle system

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import Model.Character;
 import Service.*;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         Character[] characters = {
                 new Warrior("Arthur", 10, 100, 50),
@@ -20,28 +23,173 @@ public class Main {
 
         Enemy currentEnemy = enemies[0];
 
-        System.out.println("=== CHARACTER LIST ===\n");
+        while (currentEnemy.isAlive()) {
 
-        for (Character c : characters) {
-
-            if (!c.isAlive()) {
-                continue;
-            }
-
-            c.showStatus();
-
-            int damage = c.attack();
-
-            currentEnemy.takeDamage(damage);
-
+            System.out.println("\n=== BATTLE ===");
             System.out.println(
                     currentEnemy.getName() +
                             " HP: " +
                             currentEnemy.getHealth()
             );
 
-            if (currentEnemy.isAlive()) {
+            for (Character c : characters) {
 
+                if (!c.isAlive()) {
+                    continue;
+                }
+
+                c.showStatus();
+
+                int damage = 0;
+
+                System.out.println(
+                        "\n===== " +
+                                c.getName() +
+                                "'s Turn ====="
+                );
+
+                if (c instanceof Warrior) {
+
+                    Warrior w = (Warrior) c;
+
+                    System.out.println("1. Attack");
+                    System.out.println("2. Activate Rage");
+
+                    int choice = sc.nextInt();
+
+                    switch (choice) {
+
+                        case 1:
+                            damage = w.attack();
+                            break;
+
+                        case 2:
+                            w.activateRage();
+                            break;
+
+                        default:
+                            System.out.println("Invalid choice!");
+                    }
+                }
+
+                if (c instanceof Archer) {
+
+                    Archer a = (Archer) c;
+
+                    System.out.println("1. Attack");
+                    System.out.println("2. Reload Arrows");
+
+                    int choice = sc.nextInt();
+
+                    switch (choice) {
+
+                        case 1:
+                            damage = a.attack();
+                            break;
+
+                        case 2:
+                            a.reloadArrows();
+                            break;
+
+                        default:
+                            System.out.println("Invalid choice!");
+                    }
+                }
+
+                if (c instanceof DragonKnight) {
+
+                    DragonKnight dk = (DragonKnight) c;
+
+                    System.out.println("1. Attack");
+                    System.out.println("2. Summon Dragon");
+
+                    int choice = sc.nextInt();
+
+                    switch (choice) {
+
+                        case 1:
+                            damage = dk.attack();
+                            break;
+
+                        case 2:
+                            dk.summonDragon();
+                            break;
+
+                        default:
+                            System.out.println("Invalid choice!");
+                    }
+                }
+
+                if (c instanceof Mage) {
+
+                    Mage m = (Mage) c;
+
+                    System.out.println("1. Attack");
+                    System.out.println("2. Cast Ultimate Spell");
+
+                    int choice = sc.nextInt();
+
+                    switch (choice) {
+
+                        case 1:
+                            damage = m.attack();
+                            break;
+
+                        case 2:
+                            m.castUltimateSpell();
+                            break;
+
+                        default:
+                            System.out.println("Invalid choice!");
+                    }
+                }
+
+                if (c instanceof Healer) {
+
+                    Healer h = (Healer) c;
+
+                    System.out.println("1. Attack");
+                    System.out.println("2. Revive Ally");
+
+                    int choice = sc.nextInt();
+
+                    switch (choice) {
+
+                        case 1:
+                            damage = h.attack();
+                            break;
+
+                        case 2:
+                            h.reviveAlly();
+                            break;
+
+                        default:
+                            System.out.println("Invalid choice!");
+                    }
+                }
+
+                // Character attacks enemy
+                currentEnemy.takeDamage(damage);
+
+                System.out.println(
+                        currentEnemy.getName() +
+                                " HP: " +
+                                currentEnemy.getHealth()
+                );
+
+                // Enemy defeated
+                if (!currentEnemy.isAlive()) {
+
+                    System.out.println(
+                            "\n" +
+                                    currentEnemy.getName() +
+                                    " has been defeated!"
+                    );
+
+                    break;
+                }
+
+                // Enemy attacks back
                 c.takeDamage(currentEnemy.getDamage());
 
                 System.out.println(
@@ -55,74 +203,11 @@ public class Main {
                                 " HP: " +
                                 c.getHealth()
                 );
+
+                System.out.println("--------------------");
             }
-
-            if (c instanceof Warrior) {
-                Warrior w = (Warrior) c;
-
-                System.out.println(
-                        "Stamina: " +
-                                w.getStamina()
-                );
-
-                w.activateRage();
-            }
-
-            if (c instanceof Archer) {
-                Archer a = (Archer) c;
-
-                System.out.println(
-                        "Stamina: " +
-                                a.getStamina()
-                );
-
-                a.reloadArrows();
-            }
-
-            if (c instanceof DragonKnight) {
-                DragonKnight dk = (DragonKnight) c;
-
-                System.out.println(
-                        "Stamina: " +
-                                dk.getStamina()
-                );
-
-                dk.summonDragon();
-            }
-
-            if (c instanceof Mage) {
-                Mage m = (Mage) c;
-
-                System.out.println(
-                        "Mana: " +
-                                m.getMana()
-                );
-
-                m.castUltimateSpell();
-            }
-
-            if (c instanceof Healer) {
-                Healer h = (Healer) c;
-
-                System.out.println(
-                        "Mana: " +
-                                h.getMana()
-                );
-
-                h.reviveAlly();
-            }
-
-            if (!currentEnemy.isAlive()) {
-
-                System.out.println(
-                        "\n" + currentEnemy.getName() +
-                                " has been defeated!"
-                );
-
-                break;
-            }
-
-            System.out.println("--------------------");
         }
+
+        sc.close();
     }
 }
